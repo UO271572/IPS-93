f93f046690f7c35b0e69708bcf5907bff6956d37
UO277653 <UO277653@EII-L15-02.mshome.net>
2021-10-14 18:55:02 +0200

    Primer commit

--- src/main/java/ips/business/corredores/CorredoresController.java
+++ src/main/java/ips/business/corredores/CorredoresController.java
@@ -28,5 +28,43 @@ public class CorredoresController {
 		List<CorredorDTO> corredores=model.getListaCorredoresByIdCarrera(idCarrera);
 		return corredores;
 	}
+	
+	public String getCategoría(String genero, int edad) {
+		
+		String res = "";
+		
+		if(genero.toLowerCase().equals("hombre")) {
+			res += getCategoríaEdad(edad) + " - Masculino";
+		} else if(genero.toLowerCase().equals("mujer")) {
+			res += getCategoríaEdad(edad) + " - Femenino";
+		} else {
+			throw new RuntimeException("Error: género no válido");
+		}
+		
+		return res;
+	}
+
+	private String getCategoríaEdad(int edad) {
+		
+		String res = "";
+		
+		if(edad>40) {
+			res += "Adulto - Mayor de 40 años";
+		} else if (edad >= 35) {
+			res += "Adulto - Entre 35 y 39 años";
+		} else if (edad >= 30) {
+			res += "Mayor - Entre 30 y 34 años";
+		} else if (edad >= 25) {
+			res += "Mayor - Entre 25 y 29 años";
+		} else if (edad >= 20) {
+			res += "Juvenil - Entre 20 y 24 años";
+		} else if (edad >= 15) {
+			res += "Juvenil - Entre 15 y 19 años";
+		} else if (edad >= 10) {
+			res += "Infantil - Entre 10 y 14 años";
+		}
+			
+		return res;
+	}
 
 }
